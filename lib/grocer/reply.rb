f41b49da239f9fb7ee54808fdc385ff0382a986c
John Naegle <john.naegle@goodmeasures.com>
2013-01-15 11:13:15 -0600

    Add description of reply status codes

--- lib/grocer/reply.rb
+++ lib/grocer/reply.rb
@@ -3,7 +3,26 @@ module Grocer
     LENGTH = 6
 
     attr_accessor :identifier, :command, :status
-
+    
+    def error?
+      status > 0
+    end
+    
+    def describe
+      {
+        0 => "No Errors",
+        1 => "Processing Error",
+        2 => "Missing Device Token",
+        3 => "Missing Topic",
+        4 => "Missing Payload",
+        5 => "Invalid Token Size",
+        6 => "Invalid Topic Size",
+        7 => "Invalid Payload Size",
+        8 => "Invalid Token",
+        255 => "None (Unknown)"
+      }[status]
+    end
+    
     def initialize(binary_tuple)
       # C   =>  1 byte command
       # C   =>  1 byte statis
